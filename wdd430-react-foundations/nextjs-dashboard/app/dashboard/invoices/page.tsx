8b0ad348623706fbfc40f4407c77997cce8b66b3
WTHACD <walejandrocamacho@gmail.com>
2025-11-18 01:26:42 -0600

    10

--- wdd430-react-foundations/nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ wdd430-react-foundations/nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -1,30 +1,23 @@
-import { Metadata } from 'next';
 import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { fetchInvoicesPages } from '@/app/lib/data';
-import { Suspense } from 'react';
- 
-export const metadata: Metadata = {
-  title: 'Invoices',
-};
-export const dynamic = 'force-dynamic';
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams?: {
+export default async function Page(props: {
+  searchParams?: Promise<{
     query?: string;
     page?: string;
-  };
+  }>;
 }) {
+  const searchParams = await props.searchParams;
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
   const totalPages = await fetchInvoicesPages(query);
-
+ 
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -34,11 +27,11 @@ export default async function Page({
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        <Pagination totalPages={totalPages} />
+        { <Pagination totalPages={totalPages} />}
       </div>
     </div>
   );
